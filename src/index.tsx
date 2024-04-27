e22674c1d5ea854a7e77829f3fc7c60480b6d5e1
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2024-04-27 16:46:55 -0700

    section 5-9

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a reference to the div with ID root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      define: {
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        global: "window",
+      },
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        cols={80}
+        rows={30}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+root.render(<App />);
